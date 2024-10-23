6443022f843b137b7eae77fd195ea6bfac3af87d
unknown <ebubeidika@gmail.com>
2024-10-24 00:13:31 +0100

    wowo

--- posserver/settings.py
+++ posserver/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,6 +145,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+# Where the static files will be collected
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Add your static file directories if needed
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Your static folder
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
